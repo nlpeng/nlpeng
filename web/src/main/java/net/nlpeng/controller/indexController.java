b4fabc1244b80e82e28b2e7e84235f32624be40c
nlpeng <nlpeng@outlook.com>
2017-09-15 00:24:03 +0800

    1.0.1版重新配置spring

---
+++ web/src/main/java/net/nlpeng/controller/indexController.java
@@ -0,0 +1,16 @@
+package net.nlpeng.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by nlpeng on 2017/9/14.
+ */
+@Controller
+public class indexController {
+
+    @RequestMapping("/index")
+    public String index(){
+        return "home";
+    }
+}
---
+++ web/src/main/webapp/WEB-INF/jsp/home.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: nlpeng
+  Date: 2017/9/14
+  Time: 23:26
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>NLpeng</title>
+</head>
+<body>
+<h4>这是个新项目</h4>
+</body>
+</html>
